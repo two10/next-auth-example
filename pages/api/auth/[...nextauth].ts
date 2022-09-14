bb43479a0ab1cf50eb054aaa1d620a238d33a427
Puneet Jindal <puneet@adyogi.com>
2022-09-14 18:24:57 +0530

    added email provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,6 +4,7 @@ import FacebookProvider from "next-auth/providers/facebook"
 import GithubProvider from "next-auth/providers/github"
 import TwitterProvider from "next-auth/providers/twitter"
 import Auth0Provider from "next-auth/providers/auth0"
+import EmailProvider from "next-auth/providers/email"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 // import AppleProvider from "next-auth/providers/apple"
@@ -15,15 +16,16 @@ const prisma = new PrismaClient()
 export const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/providers/oauth
   adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
   providers: [
    
-    /* EmailProvider({
+     EmailProvider({
          server: process.env.EMAIL_SERVER,
          from: process.env.EMAIL_FROM,
        }),
     // Temporarily removing the Apple provider from the demo site as the
     // callback URL for it needs updating due to Vercel changing domains
-
+/*
     Providers.Apple({
       clientId: process.env.APPLE_ID,
       clientSecret: {
